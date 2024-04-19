c97173013b5f10fe2774efa1a6acbc5affad8c45
jngouna <jenniferfresnay@gmail.com>
2024-04-20 01:54:05 +0200

    suppression

--- projetBDDR/investigation/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AddresseEmail, Email,Employee, ReceiversMail
-
-# Register your models here.
-admin.site.register(Email)
-admin.site.register(Employee)
-admin.site.register(ReceiversMail)
-admin.site.register(AddresseEmail)
